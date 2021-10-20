1bf4f18b67824fb4cbe6c9cb29a7a89a2ad6b9a1
Andre gois <andre.s.gois3@gmail.com>
2021-10-20 19:57:24 -0300

    writing raw queries

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -5,26 +5,27 @@ import { User } from '../../entities/User';
 import { IUsersRepository } from '../IUsersRepository';
 
 export class UsersRepository implements IUsersRepository {
+  
   private repository: Repository<User>;
 
   constructor() {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
+  async findUserWithGamesById({user_id,}: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOneOrFail((user_id));
+    return user;
   }
 
-  async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+ findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users AS u WHERE ORDER BY first_name'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM users WHERE first_name=${first_name} and last_name=${last_name}`); // Complete usando raw query
   }
 }
